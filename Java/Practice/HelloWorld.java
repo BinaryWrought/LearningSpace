c9ba9791c6a9c2b8641a670eae41fe4dffafbe32
BinaryWrought <v1d30g8mr@gmail.com>
2017-09-15 14:42:49 -0700

    Adding practice files
    Adding practice files under new directory structure.
    Because I made a folder specifically for GFG solutions under the "School" difficulty, I thought it'd be good to make a "Practice" directory for solutions to GFG practice challenges.

---
+++ Java/Practice/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * This is Hello World done in Java.
+ *
+ * @author <a href="mailto:jhazelle@asu.edu">Justin Hazelle</a>
+ * <a href="https://github.com/BinaryWrought" target="_blank">GitHub</a>
+ */
+class HelloWorld
+{
+	/**
+	The main method is the entry point to any Java program.
+	@param args Is a list of Strings that can be passed to the program when it runs.
+	*/
+    public static void main( String args[] )
+    {
+        System.out.println( "Hello World from Java" );
+    }
+}
